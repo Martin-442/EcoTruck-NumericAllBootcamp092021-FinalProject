53ced4a092dea200dbc27d2df2ae05efec523047
iqqu1994 <iqqu1994@gmail.com>
2021-08-30 09:11:27 +0200

    role complited

--- resources/views/role-provider/dashboard-provider.blade.php
+++ resources/views/role-provider/dashboard-provider.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboard Provider') }}
         </h2>
     </x-slot>
 
@@ -12,22 +12,10 @@
                     Provider
                 </div>
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Create Equiment: <a href="{{ URL::route('equipment-new') }}"></a>
+                    Create Equiment: <a href=""></a>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
-
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-
-    </x-auth-card>
-</x-guest-layout>
 
