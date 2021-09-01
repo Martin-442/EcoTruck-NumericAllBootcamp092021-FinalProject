2f1e268c519bfea14a92f65f87da07d17a0c4fc2
Martin-442 <martin.442@icloud.com>
2021-09-01 23:12:58 +0200

    antd 1st try

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="">
 </head>
 <body>
 <h1>Welcome to ECO Truck Pooling</h1>
@@ -13,5 +14,6 @@
 <!-- <a href="{{ URL::route('register') }}">Admin</a><br> -->
 <br>
 <a href="{{ URL::route('login') }}">Login</a>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/antd/4.17.0-alpha.0/antd.min.js"></script>
 </body>
 </html>
