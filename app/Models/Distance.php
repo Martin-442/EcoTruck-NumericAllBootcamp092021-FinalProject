a1a7e266f54075261a1ef5267f7bc3145bcf2780
CordingMart <85163909+CordingMart@users.noreply.github.com>
2021-09-02 19:02:32 +0200

    Geolocations to stop table (incl. StopSeeder)
    https://data.public.lu/en/datasets/liste-des-localites-avec-coordonnees/#_

--- app/Models/Distance.php
+++ app/Models/Distance.php
@@ -14,11 +14,7 @@ class Distance extends Model
     protected $fillable = ['id', 'lengths_json'];
 
     public function getLengthAttribute($from_stop_id, $to_stop_id) {
-<<<<<<< Updated upstream
-        if($from_stop_id == $to_stop_id) return 0; // 0 length if both id's are the same
-=======
         if ($from_stop_id == $to_stop_id) return 0; // 0 length if both id's are the same
->>>>>>> Stashed changes
         $distance = Distance::where('id', '=', $from_stop_id)->first();
         foreach (json_decode($distance->lengths_json) as $value) {
             if ($value->to_stop_id == $to_stop_id) {
